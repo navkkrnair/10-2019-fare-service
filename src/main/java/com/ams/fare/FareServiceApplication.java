dbfec85ea2caa8a354541df14211d1a2e933f0be
navkk <navkkrnair@gmail.com>
2019-10-24 11:25:35 +0530

    Changed code

--- src/main/java/com/ams/fare/FareServiceApplication.java
+++ src/main/java/com/ams/fare/FareServiceApplication.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.ams.fare.entity.Fare;
@@ -20,13 +19,12 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @SpringBootApplication
-@EnableDiscoveryClient
 public class FareServiceApplication
 {
 	private static final Logger logger = LoggerFactory.getLogger(FareServiceApplication.class);
 
 	private final FareRepository fareRepository;
-	
+
 	@Bean
 	public Sampler defaultSampler()
 	{
@@ -43,18 +41,26 @@ public class FareServiceApplication
 	{
 		return args ->
 		{
-			List<Fare> list = Arrays.asList(new Fare("BF100", LocalDate
-				.of(2019, 01, 22), "100"), new Fare("BF101", LocalDate
-					.of(2019, 01, 22), "101"), new Fare("BF102", LocalDate
-						.of(2019, 01, 22), "102"), new Fare("BF103", LocalDate
-							.of(2019, 01, 22), "103"), new Fare("BF104", LocalDate
-								.of(2019, 01, 22), "104"), new Fare("BF105", LocalDate
-									.of(2019, 01, 22), "105"), new Fare("BF106", LocalDate
-										.of(2019, 01, 22), "106"));
+			List<Fare> list = Arrays.asList(
+					new Fare("BF100", LocalDate
+						.of(2019, 01, 22), "100"),
+						new Fare("BF101", LocalDate
+							.of(2019, 01, 22), "101"),
+						new Fare("BF102", LocalDate
+							.of(2019, 01, 22), "102"),
+						new Fare("BF103", LocalDate
+							.of(2019, 01, 22), "103"),
+						new Fare("BF104", LocalDate
+							.of(2019, 01, 22), "104"),
+						new Fare("BF105", LocalDate
+							.of(2019, 01, 22), "105"),
+						new Fare("BF106", LocalDate
+							.of(2019, 01, 22), "106"));
 			list.forEach(fare -> this.fareRepository.save(fare));
 
-			logger.info("Result: " + this.fareRepository
-				.getFareByFlightNumberAndFlightDate("BF101", LocalDate.of(2019, 01, 22)));
+			logger.info(
+					"Result: " + this.fareRepository
+						.getFareByFlightNumberAndFlightDate("BF101", LocalDate.of(2019, 01, 22)));
 
 		};
 	}
